ea500c078e025c67e817b82b3744da2322409b4f
ginoasuncion <ginoasuncion@gmail.com>
2024-05-09 14:53:19 +0700

    add welcome message with reply keyboard markup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler
 from dotenv import load_dotenv
 import os
@@ -8,10 +8,12 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 async def start(update: Update, context):
-    await update.message.reply_text('Sawadee khrap')
+    welcome_message = "Sawadee Harbour Space students! Welcome to GiniBot, your pocket guide to awesome places and essentials in Bangkok, Thailand. Please select a place to go below, and I'll recommend some great options for you."
+    markup = ReplyKeyboardMarkup([['Cafe', 'Restaurant', 'Hospital']])
+    await update.message.reply_text(welcome_message, reply_markup=markup)
 
 def main():
-    app = Application.builder().token(BOT_TOKEN ).build()
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler(command='start', callback=start))
     app.run_polling()
 
